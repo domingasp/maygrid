2054178595c1c6e5123aa9d7748044a284b05ebd
Domingas Pleckauskas <domingas@Domingass-MacBook-Pro.local>
2020-01-16 22:02:07 +0000

    Adding boiler plate code.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php ?><!DOCTYPE html>
+<html>
+    <head>
+        <meta name = "viewport" content = "width = device-width, initial-scale = 1, maximum-scale = 1">
+
+        <link rel="icon" href="./assets/maygrid_logo.png">
+        <link rel = "stylesheet" type = "text/css" href = "./css/stylesheet.css">
+    
+        <title>Maygrid</title>
+    </head>
+
+    <body>
+    
+
+        <script src = "./js/script.js"></script>
+    </body>
+</html>
