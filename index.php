0ca249e9d903d9be8b2f0717e5ce39cbe83046f5
Domingas Pleckauskas <domingas@Domingass-MacBook-Pro.local>
2020-01-17 10:03:21 +0000

    Added header; logo and add button.

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
         <meta name = "viewport" content = "width = device-width, initial-scale = 1, maximum-scale = 1">
 
         <link rel="icon" href="./assets/maygrid_logo.png">
+        <link rel = "stylesheet" type = "text/css" href = "./css/all.css">
         <link rel = "stylesheet" type = "text/css" href = "./css/stylesheet.css">
     
         <title>Maygrid</title>
     </head>
 
     <body>
-    
+        <div class="header-main-div">
+            <img class="header-logo" src="./assets/maygrid_logo.png" alt="Maygrid logo" />
+            <button class="header-btn"><i class="fas fa-plus"></i></button>
+        </div>
 
         <script src = "./js/script.js"></script>
     </body>
