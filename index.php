ebcd9d5f68ad01fc5a58fd2627d4910dcfdb2793
Domingas Pleckauskas <domingas@Domingass-MacBook-Pro.local>
2020-01-17 11:11:29 +0000

    Added sample cards and styling.

--- index.php
+++ index.php
@@ -16,6 +16,58 @@
             <button class="header-btn"><i class="fas fa-plus"></i></button>
         </div>
 
+        <div class="card-board-div">
+
+            <div class="card-div" style="background-color: #ffffff">
+                <span class="card-content"> is o this is o this is o this is o this is o this is o this is o this is o this is o this is o this is o this is o this is o this is o this is o</span>
+                <div class="card-likes-div">
+                    <button class="card-like-btn"><i class="fas fa-heart"></i></button>
+                    <span class="card-likes">0 likes</span>
+                </div>
+            </div>
+
+            <div class="card-div" style="background-color: #ffffff">
+                <span class="card-content">this is ois is o this is o this is o this is o this is o this is o this is o this is o this is o this is o this is o this is o</span>
+                <div class="card-likes-div">
+                    <button class="card-like-btn"><i class="fas fa-heart"></i></button>
+                    <span class="card-likes">0 likes</span>
+                </div>
+            </div>
+
+            <div class="card-div" style="background-color: #ffffff">
+                <span class="card-content">this is o this is o this is o this is o this is o this is o thiss o this is o this is o this is o this is o this is o this is o this is o this is o this is o</span>
+                <div class="card-likes-div">
+                    <button class="card-like-btn"><i class="fas fa-heart"></i></button>
+                    <span class="card-likes">0 likes</span>
+                </div>
+            </div>
+
+            <div class="card-div" style="background-color: #ffffff">
+                <span class="card-content"><img src="./assets/maygrid_logo.png" /></span>
+                <div class="card-likes-div">
+                    <button class="card-like-btn"><i class="fas fa-heart"></i></button>
+                    <span class="card-likes">0 likes</span>
+                </div>
+            </div>
+
+            <div class="card-div" style="background-color: #ffffff">
+                <span class="card-content">this is o thiss o this is o this isis o this is o this is o this is o this is o this is o this is o this is o this is o this is o this is o this is o</span>
+                <div class="card-likes-div">
+                    <button class="card-like-btn"><i class="fas fa-heart"></i></button>
+                    <span class="card-likes">0 likes</span>
+                </div>
+            </div>
+
+            <div class="card-div" style="background-color: #ffffff">
+                <span class="card-content"><img src="./assets/maygrid_logo.png" /></span>
+                <div class="card-likes-div">
+                    <button class="card-like-btn"><i class="fas fa-heart"></i></button>
+                    <span class="card-likes">0 likes</span>
+                </div>
+            </div>
+
+        </div>
+
         <script src = "./js/script.js"></script>
     </body>
 </html>
