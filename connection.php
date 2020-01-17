8a1da4949a83b9727d026b224b92bdb184123f48
Domingas Pleckauskas <domingas@Domingass-MacBook-Pro.local>
2020-01-17 10:18:55 +0000

    Added a connection to the database.

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$server_name = "localhost";
+$username = "root";
+$password = "password";
+$database = "maygrid";
+
+$conn = mysqli_connect($server_name, $username, $password, $database);
+
+if ($conn -> connect_error) {
+    die("Connection failed: " . $conn -> connect_error);
+}
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php ?><!DOCTYPE html>
+<?php include ("connection.php"); ?><!DOCTYPE html>
 <html>
     <head>
         <meta name = "viewport" content = "width = device-width, initial-scale = 1, maximum-scale = 1">
